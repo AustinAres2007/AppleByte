c7c1268f0bdf2964411ee6bdf3b709d5ecdeea36
Austin Ares <hakuares@gmail.com>
2022-08-21 16:39:18 +0100

    Added on_ready and setup_hook listeners

--- src/applebytes.py
+++ src/applebytes.py
@@ -8,12 +8,19 @@ PREFIX = ">"
 BOT_NAME = "AppleByte"
 TOKEN = "MTAxMDkxNTc1ODU0MjQyNjE0Mg.GmK2gI.Oh_2tMaLXmivvmHDCxbngh53G7g0xvXXGPXdeU"
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
 client = commands.AutoShardedBot(PREFIX, intents=intents)
 
 class AppleByteClient(commands.Bot):
     def __init__(self):
         pass
+
+    def on_ready(self):
+        pass
+    
+    async def setup_hook(self):
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{PREFIX}"))
+    
 async def main():
     await client.start(TOKEN)
 
